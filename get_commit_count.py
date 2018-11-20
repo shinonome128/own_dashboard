c50ad19e0d45c4828a8831a40191d32626d532c2
shinonome128 <shinonome128@gmail.com>
2018-11-21 03:41:06 +0900

    Change url to commit information acquisition

--- get_commit_count.py
+++ get_commit_count.py
@@ -1,36 +1,46 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import os, json, functools
-# from urllib2 import urlopen, Request
 from urllib.request import urlopen, Request
 
-# GitHub Traffic API
-# https://developer.github.com/v3/repos/traffic/
-def openTrafficAPI (owner, repo, token, path):
-    url = "https://api.github.com/repos/%s/%s/traffic/%s" % (owner, repo, path)
+def openTrafficAPI (owner, token, repo):
+    ## 日付を取得してパラメータで渡す、後でやる
+    url = "https://api.github.com/repos/%s/%s/commits" % (owner, repo)
     headers = {
         'Authorization': 'token %s' % token,
         'Accept': 'application/vnd.github.spiderman-preview'
     }
     return urlopen (Request (url, headers=headers))
 
-# 取ってくるデーター
-paths = [
-    'popular/referrers',
-    'popular/paths',
-    'views',
-    'clones',
-]
+## レポジトリの一覧を取得して配列に格納、あとでやる
+
+repo = [
+    'own_dashboard',
+    'autologin',
+    'cicddemo',
+    'detectcat',
+    'devops-example-client',
+    'devops-example-server',
+    'gcptest',
+    'gvim',
+    'gwcheck',
+    'jr',
+    'knowledge',
+    'lteanalysis',
+    'own_dashboard',
+    'trello_move_green_to_clip',
+    'trello_move_green_to_plan',
+    ]
+
+## 日付を生成、あとでやる
 
 connections = []
 try:
     # openTrafficAPI関数の部分適用 (認証情報を環境変数から得て)
+    # 日付情報も後で渡す、あとでやる
     fAPI = functools.partial (openTrafficAPI,
                                 os.environ['GITHUB_OWNER'],
-                                os.environ['GITHUB_REPO'],
                                 os.environ['GITHUB_TOKEN'])
     # open connections
-    connections = [fAPI (p) for p in paths]
+    connections = [fAPI (r) for r in repo]
     # JSONパース
     datadicts = [json.loads (c.read ()) for c in connections]
 finally:
@@ -40,5 +50,6 @@ del connections
 
 # 辞書型のデーターをダンプ
 dumps = [json.dumps (d, indent=2) for d in datadicts]
+
 # 結合
 print ("\n".join (dumps))
