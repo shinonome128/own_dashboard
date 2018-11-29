dcab9d90a39a34ed6378165bc76c27c556c9e966
shinonome128 <shinonome128@gmail.com>
2018-11-29 17:26:54 +0900

    Delete main.py

--- deploy.bat
+++ deploy.bat
@@ -5,6 +5,7 @@ REM deploy.bat
 REM ///////////////////////////////////////////////////
 
 cd C:\Users\shino\doc\own_dashboard
-gcloud beta functions deploy main --region=us-central1 --runtime=python37 --env-vars-file conf.yml --source=C:\Users\shino\doc\own_dashboard --trigger-http
+REM gcloud beta functions deploy main --region=us-central1 --runtime=python37 --env-vars-file conf.yml --source=C:\Users\shino\doc\own_dashboard --trigger-http
+gcloud beta functions deploy main --region=us-central1 --runtime=python37 --source=C:\Users\shino\doc\own_dashboard --trigger-http
 
 Pause
