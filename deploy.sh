f594446ac66a1b08c751f93ea7985d05385f4d3a
shinonome128 <shinonome128@gmail.com>
2019-02-11 16:21:32 +0900

    Add gcloud auth command

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+gcloud auth activate-service-account --key-file gcf-demo-2b39da7a07dd.json  
 gcloud beta functions deploy main --region=us-central1 --runtime=python37 --env-vars-file conf.yml --source=./ --trigger-http
