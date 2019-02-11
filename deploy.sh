5c6644924b1377652fe7065f6b063fb8a52ffb1c
shinonome128 <shinonome128@gmail.com>
2019-02-11 16:39:27 +0900

    Add changing directory

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-pwd
+cd /home/travis/build/shinonome128/own_dashboard
 gcloud auth activate-service-account --key-file gcf-demo-2b39da7a07dd.json  
 gcloud beta functions deploy main --region=us-central1 --runtime=python37 --env-vars-file conf.yml --source=./ --trigger-http
