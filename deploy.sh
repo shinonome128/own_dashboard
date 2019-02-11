d3e86682372ee53226ba5e5d3486a651b8f8ae6b
shinonome128 <shinonome128@gmail.com>
2019-02-11 16:55:34 +0900

    Add debug

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 cd /home/travis/build/shinonome128/own_dashboard
+ls -la
 gcloud auth activate-service-account --key-file gcf-demo-2b39da7a07dd.json  
 gcloud beta functions deploy main --region=us-central1 --runtime=python37 --env-vars-file conf.yml --source=./ --trigger-http
