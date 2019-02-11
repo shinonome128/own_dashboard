12d3669e39d5608d0d82c3f4dab91d7e1311679d
shinonome128 <shinonome128@gmail.com>
2019-02-11 16:32:41 +0900

    Add debug command

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+pwd
 gcloud auth activate-service-account --key-file gcf-demo-2b39da7a07dd.json  
 gcloud beta functions deploy main --region=us-central1 --runtime=python37 --env-vars-file conf.yml --source=./ --trigger-http
